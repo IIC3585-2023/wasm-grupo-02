e8545457d7a66f7a43509046ac39daa427e835ef
José Tomás Jiménez <josejimenezp@icloud.com>
2023-04-25 20:17:17 -0400

    front v1

--- main.js
+++ main.js
@@ -25,3 +25,97 @@ Module.onRuntimeInitialized = () => {
   
     Module._free(clustersPtr);
   };
+
+const clustersInput = document.getElementById("clusters");
+const clustersTbody = document.getElementById("clusters-body");
+
+clustersInput.addEventListener("input", actualizarClusters);
+
+
+
+const trabajosInput = document.getElementById("trabajos");
+const trabajosTbody = document.getElementById("trabajos-body");
+trabajosInput.addEventListener("input", actualizarTrabajos);
+
+// Añadimos una fila por defecto con un id único
+const row = document.createElement("tr");
+row.id = "trabajo-1";
+const trabajoCell = document.createElement("td");
+trabajoCell.textContent = `Trabajo 1`;
+row.appendChild(trabajoCell);
+const tiempoCell = document.createElement("td");
+const input = document.createElement("input");
+input.type = "number";
+input.min = "1";
+input.value = Math.ceil(Math.random() * 10);
+tiempoCell.appendChild(input);
+row.appendChild(tiempoCell);
+trabajosTbody.appendChild(row);
+
+function actualizarTrabajos() {
+  const numTrabajos = trabajosInput.value;
+  const rows = trabajosTbody.getElementsByTagName("tr");
+  let numRows = rows.length;
+
+  // Actualizamos las celdas existentes y creamos nuevas filas y celdas de tiempo de asignación para la tabla de trabajos
+  for (let i = 1; i <= numTrabajos; i++) {
+    let row;
+    let tiempoCell;
+    let input;
+
+    if (i <= numRows) { // Si la fila ya existe, la actualizamos
+      row = i == 1 ? document.getElementById("trabajo-1") : rows[i-1];
+      tiempoCell = row.getElementsByTagName("td")[1];
+      input = tiempoCell.getElementsByTagName("input")[0];
+    } else { // Si la fila no existe, la creamos
+      row = document.createElement("tr");
+      const trabajoCell = document.createElement("td");
+      trabajoCell.textContent = `Trabajo ${i}`;
+      row.appendChild(trabajoCell);
+      tiempoCell = document.createElement("td");
+      input = document.createElement("input");
+      input.type = "number";
+      input.min = "1";
+      tiempoCell.appendChild(input);
+      row.appendChild(tiempoCell);
+      trabajosTbody.appendChild(row);
+    }
+
+    // Si la celda es nueva, le asignamos un valor aleatorio
+    if (i > numRows) {
+      input.value = Math.ceil(Math.random() * 10);
+    }
+  }
+
+  // Eliminamos las filas sobrantes
+  while (trabajosTbody.getElementsByTagName("tr").length > numTrabajos) {
+    trabajosTbody.removeChild(trabajosTbody.lastChild);
+  }
+}
+
+
+
+
+function actualizarClusters() {
+  const numClusters = clustersInput.value;
+
+  // Eliminamos todas las filas existentes de la tabla de clusters
+  while (clustersTbody.firstChild) {
+    clustersTbody.removeChild(clustersTbody.firstChild);
+  }
+
+  // Creamos las nuevas filas y celdas de tiempo de asignación para la tabla de clusters
+  for (let i = 1; i <= numClusters; i++) {
+    const row2 = document.createElement("tr");
+    const clusterCell = document.createElement("td");
+    clusterCell.textContent = `Cluster ${i}`;
+    row2.appendChild(clusterCell);
+    const tiempoCell2 = document.createElement("td");
+    const button = document.createElement("button");
+    button.type = "button";
+    button.textContent = "Ver tareas asignadas";
+    tiempoCell2.appendChild(button);
+    row2.appendChild(tiempoCell2);
+    clustersTbody.appendChild(row2);
+  }
+}
--- scripts/function.c
+++ scripts/function.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <emscripten.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <emscripten.h>
 
 
-EMSCRIPTEN_KEEPALIVE
+// EMSCRIPTEN_KEEPALIVE
 
-// Modificar la firma de la función optimize
-int** optimize(int* times, int times_length, int numClusters) {
-    int** clusters = (int**)malloc(numClusters * sizeof(int*));
-    int* clustersTimes = (int*)calloc(numClusters, sizeof(int));
+// // Modificar la firma de la función optimize
+// int** optimize(int* times, int times_length, int numClusters) {
+//     int** clusters = (int**)malloc(numClusters * sizeof(int*));
+//     int* clustersTimes = (int*)calloc(numClusters, sizeof(int));
 
-    for (int i = 0; i < numClusters; i++) {
-        clusters[i] = (int*)malloc(times_length * sizeof(int));
-    }
+//     for (int i = 0; i < numClusters; i++) {
+//         clusters[i] = (int*)malloc(times_length * sizeof(int));
+//     }
 
-    for (int i = 0; i < times_length; i++) {
-        int time = times[i];
-        int minTime = 1000000;
-        int clusterId;
+//     for (int i = 0; i < times_length; i++) {
+//         int time = times[i];
+//         int minTime = 1000000;
+//         int clusterId;
 
-        for (int id = 0; id < numClusters; id++) {
-            int totalTiempo = clustersTimes[id];
-            if (totalTiempo < minTime) {
-                minTime = totalTiempo;
-                clusterId = id;
-            }
-        }
+//         for (int id = 0; id < numClusters; id++) {
+//             int totalTiempo = clustersTimes[id];
+//             if (totalTiempo < minTime) {
+//                 minTime = totalTiempo;
+//                 clusterId = id;
+//             }
+//         }
 
-        clusters[clusterId][i] = i + 1;
-        clustersTimes[clusterId] = minTime + time;
-        printf("Tiempo: %d, clusterId: %d, minTime: %d\n", time, clusterId, minTime);
-    }
+//         clusters[clusterId][i] = i + 1;
+//         clustersTimes[clusterId] = minTime + time;
+//         printf("Tiempo: %d, clusterId: %d, minTime: %d\n", time, clusterId, minTime);
+//     }
 
-    free(clustersTimes);
-    return clusters;
-}
+//     free(clustersTimes);
+//     return clusters;
+// }
 
 
